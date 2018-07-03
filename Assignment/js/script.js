94d37e291fd7a807dc962c0aa2ad96fe02673dfc
KirtiBihari <bihari.kirti@gmail.com>
2018-07-03 10:30:50 +0000

    Assignment - HTML5 and CSS

---
+++ Assignment/js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+$(".navlist-icon").click(function(){
+    if(!$(".navlist-mobile").is(":visible")){
+        $(".navlist-mobile").show();
+    }else{
+        $(".navlist-mobile").hide();
+    }
+    
+});
+
+});
